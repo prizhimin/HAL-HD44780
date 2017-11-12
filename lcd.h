b47fc5380a011d7ad4f5817e26f6b05cd9a96eab
Nikolay Prizhimin <prinv@yandex.ru>
2017-11-12 10:32:59 +0500

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -6,19 +6,19 @@
 #define TRUE				1
 #define FALSE				0
 
-#define	PORT_LCD      GPIOB
+#define	PORT_LCD		GPIOB
 #define	LCD_PIN_D4		GPIO_PIN_12
 #define	LCD_PIN_D5		GPIO_PIN_15
 #define	LCD_PIN_D6		GPIO_PIN_13
 #define	LCD_PIN_D7		GPIO_PIN_14
-#define LCD_PIN_E			GPIO_PIN_9
+#define LCD_PIN_E		GPIO_PIN_9
 #define LCD_PIN_RS		GPIO_PIN_8
 
-void LCD_Init(void);	          					// Initializing the display
-void LCD_Clear(void);						          // Cleaning the display
+void LCD_Init(void);				// Initializing the display
+void LCD_Clear(void);				// Cleaning the display
 void LCD_GoTo(uint8_t Row, uint8_t Col);	// Set the cursor to the specified coordinates
-void LCD_PrintStr(uint8_t *Text);			    // Print a string
-void LCD_PrintDec(int32_t data);			    // Print a decimal number
+void LCD_PrintStr(uint8_t *Text);		// Print a string
+void LCD_PrintDec(int32_t data);		// Print a decimal number
 
 
 #endif
